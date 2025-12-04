6ab566e9354614a64ffb476553be2b238806e0c7
marko <grozdanovicmarko170@gmail.com>
2025-12-04 18:14:56 +0100

    initial commit

---
+++ src/main/java/com/secure/notes/security/request/LoginRequest.java
@@ -0,0 +1,15 @@
+package com.secure.notes.security.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class LoginRequest {
+        private String username;
+
+        private String password;
+
+}
+
+
